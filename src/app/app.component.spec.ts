444c6a79569ccb7b18f683e02fd7a2735e1b5537
Saurabh Sharma <saurabh.sharma@broadcom.com>
2019-07-30 15:42:06 +0530

    Added footer component and test case

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,12 +3,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { PersonComponent } from './shared/person/person.component';
 import { DebugElement } from '@angular/core';
-import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FooterComponent } from './footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Logger } from './shared/log.service';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBlogger, faTwitter, faGit, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 
 describe('Component AppComponent', () => {
-  let dtVar: Date;
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
   let debugElement: DebugElement;
@@ -21,9 +24,15 @@ describe('Component AppComponent', () => {
         PersonComponent,
         FooterComponent
       ],
-      imports: [RouterTestingModule]
+      providers: [Logger],
+      imports: [RouterTestingModule, FontAwesomeModule]
     }).compileComponents();
-    dtVar = new Date();
+
+    library.add(faBlogger);
+    library.add(faTwitter);
+    library.add(faGit);
+    library.add(faLinkedin);
+
   }));
 
   beforeEach(() => {
