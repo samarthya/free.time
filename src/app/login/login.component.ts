dfdf7e032d829dfc2d087ba918071a46bea47c11
Vasu  Sharma <vasusharma@beast-5.local>
2019-08-09 19:17:35 +0530

    logger issue resolved

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Logger } from 'src/app/shared';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  logger: any;
 
-  constructor() { }
+
+  constructor(private logger: Logger) { }
 
   ngOnInit() {
     this.logger.log(' ngOnInit called for LoginComponent.');
